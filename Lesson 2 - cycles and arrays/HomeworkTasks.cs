3b36a75979ed4dda7d71accbd666de3cd85e4dff
System-IOException <56886020+System-IOException@users.noreply.github.com>
2021-10-15 20:52:49 +0500

    Add homework for second lesson

---
+++ Lesson 2 - cycles and arrays/HomeworkTasks.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1.Homework
+{
+    class HomeworkTasks
+    {
+        /// <summary>
+        /// Squirrels with nuts
+        /// </summary>
+        public static void Homework1()
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int K = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(K / N);
+        }
+        /// <summary>
+        /// Last digit
+        /// </summary>
+        public static void Homework2()
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num % 10);
+        }
+        /// <summary>
+        /// Number of tens
+        /// </summary>
+        public static void Homework3()
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num % 100 / 10);
+        }
+        /// <summary>
+        /// Symmetric number
+        /// </summary>
+        public static void Homework4()
+        {
+            string num = Console.ReadLine();
+            string ret = "";
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                ret += num[i];
+            }
+            Console.WriteLine(ret);
+        }
+        /// <summary>
+        /// Snail
+        /// </summary>
+        public static void Homework5()
+        {
+            int H = Convert.ToInt32(Console.ReadLine());
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int day = 0;
+            for (int height = 0; height < H;)
+            {
+                height += A;
+                if (height >= H) break;
+                height -= B;
+                day++;
+            }
+            Console.WriteLine(day);
+        }
+    }
+}
